feb81c7d7eeb7841c3db8e1a5ca27fc6a359542c
JOEY <mariavijigeorge@gmail.com>
2023-01-03 18:12:53 +0530

    add Answer.jsx

--- auto-evaluator/pages/index.js
+++ auto-evaluator/pages/index.js
@@ -1,3 +1,19 @@
+// import AddAnswer from "../components/AddAnswer";
+// import Answerkey from "../components/answerkey";
+// import RemoveAnswer from "../components/RemoveAnswer";
+
+import AnswerList from "../components/Answer";
+
+
 export default function Home() {
-  return <></>;
+  return (
+  <div className="container mx-auto px-4  h-screen">
+    <div className="flex flex-col w-full space-y-2 my-4">
+    <h1 className="text-2xl font-bold">51 Letters</h1>
+    <h3 >Answers</h3>
+    {/* <AddAnswer />
+    <Answerkey /> */}
+    <AnswerList />
+    </div>
+  </div>);
 }
